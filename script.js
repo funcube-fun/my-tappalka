480b6d0d96ad8ad51ee6b699820467e4afffd6be
funcube-fun <dreamlandmine1@gmail.com>
2025-07-21 22:24:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -448,7 +448,7 @@ function openTab(tabName, event) {
     lastEventTime = Date.now();
     document.querySelectorAll('.tab-button').forEach(btn => btn.classList.remove('active'));
     document.querySelectorAll('.tab-content').forEach(content => content.classList.remove('active'));
-    document.querySelector(`.tab-button[data-tab="${tabName}"]`).classList.add('active');
+    document.querySelector(`.tab-button[data-tab="${tabName}"]`)?.classList.add('active');
     document.getElementById(`${tabName}-content`).classList.add('active');
 }
 
